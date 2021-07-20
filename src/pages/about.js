cede3e0b0d38a42b3f19f3e9dc4d152ea39fd8e7
unknown <tanwardurgesh@gmail.com>
2021-07-20 19:53:13 +0530

    second tutorial of the gatsby finished

---
+++ src/pages/about.js
@@ -0,0 +1,10 @@
+import * as React from "react";
+import {Link} from 'gatsby';
+import Layout from '../components/layout';
+
+const about = () =>{
+    return (<Layout pageTitle="Home page">
+    <p>Shri Krishna Govind Hare Murari Hey Nath Narayan Vasudeva</p>
+    </Layout>)
+}
+export default about;
